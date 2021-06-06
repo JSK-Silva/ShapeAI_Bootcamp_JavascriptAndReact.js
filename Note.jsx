9fe359b258b91dc97e629eaff601ae6f37719aa7
JSK-Silva <85445676+JSK-Silva@users.noreply.github.com>
2021-06-06 16:14:58 +0200

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+  return <div className = "note">
+    <h1>By M.M.J.S.K Silva</h1>
+    <p>My name is M.M.J.S.K. Silva. I have joined the 7 days Javascript and React.js bootcamp by ShapeAI. This was an amazing bootcamp and I could learn everything from Scratch including Javascript React.js and HTML</p>
+  </div>
+}
+
+export default Note;
